9087c3d12aad49998e558148ca77f95dfe80b367
marcelodeleon <mdeleoncosentino@gmail.com>
2020-10-07 19:25:28 -0300

    EL-29 EL-30 | Configured Netlify for local dev
    - Configured netlify functions to use with
      netlify dev
    
    - Configured React app to use with netlify
      dev

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://localhost:8888/.netlify/functions/hello"
           target="_blank"
           rel="noopener noreferrer"
         >
