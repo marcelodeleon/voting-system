cbcae899aafcdb02a6cd4353391b8edb901bc25d
Gonza <gonzalogg.garcia@gmail.com>
2020-10-08 01:42:48 -0300

    EL-44-voting changes

---
+++ src/components/CandidateList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class CandidateList extends React.Component {
+  render() {
+    return (
+      <ul>
+        {this.props.candidates.map((candidate) => {
+          return (
+            <li key={candidate.id}>
+              <p>{candidate.name} </p>
+              <p>{candidate.description} </p>
+              <p>{candidate.proposals[0][0]} </p>
+              <p>{candidate.proposals[0][1]} </p>
+              <p>{candidate.proposals[1][0]} </p>
+              <p>{candidate.proposals[1][1]} </p>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+}
+export default CandidateList;
--- src/components/users/Vote.js
+++ src/components/users/Vote.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import CandidateList from '../../components/CandidateList';
+import { Link } from 'react-router-dom';
 
 class Vote extends React.Component {
+  state = {
+    data: [
+      {
+        id: '1',
+        name: 'Elecciones',
+        description: 'Cambiar muebles',
+        proposals: [
+          ['Muebles nuevos', ['Si', 'No', 'Abstencion']],
+          ['Muebles viejos', ['Si', 'No', 'Abstencion']],
+        ],
+      },
+    ],
+  };
   render() {
-    return <h1>Vote</h1>;
+    return (
+      <div>
+        <CandidateList candidates={this.state.data} />
+        <Link to="/admin/election" className="btn btn-primary">
+          Votar
+        </Link>
+      </div>
+    );
   }
 }
 export default Vote;
