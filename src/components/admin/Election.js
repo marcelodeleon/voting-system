fe95aedec2b3b5b9d43b45dc04b985c2249aa249
Marcelo de León <mdeleoncosentino@gmail.com>
2020-10-08 20:12:25 -0300

    EL-38 | Create Election screen

--- src/components/admin/Election.js
+++ src/components/admin/Election.js
@@ -1,8 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
 
-class Election extends React.Component {
-  render() {
-    return <h1>Election</h1>;
-  }
+export default function Election() {
+  const [name, setName] = useState('');
+  const [startAt, setStartAt] = useState(new Date());
+  const [endAt, setEndAt] = useState(new Date());
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    /* eslint-disable-next-line */
+    console.log('Submitting');
+  };
+  return (
+    <div>
+      <h1>Nueva Eleccion</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Nombre:
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <label>
+          Descripcion:
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <label>
+          Fecha Inicio:
+          <DatePicker
+            selected={startAt}
+            onChange={(date) => setStartAt(date)}
+          />
+        </label>
+        <label>
+          Fecha Fin:
+          <DatePicker selected={endAt} onChange={(date) => setEndAt(date)} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 }
-export default Election;
