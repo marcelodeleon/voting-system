9a968d43d83bb403b9811906d4d9942db67647ab
Gonza <gonzalogg.garcia@gmail.com>
2020-10-08 00:04:57 -0300

    EL-44-update path

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Vote from './pages/Vote';
-import Election from './pages/Election';
+import Vote from './components/users/Vote';
+import Election from './components/admin/Election';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
