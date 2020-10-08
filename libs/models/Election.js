94f7b63c3c3dd7d0b89279e0fef54e251d06d8a3
marcelodeleon <mdeleoncosentino@gmail.com>
2020-10-07 23:35:10 -0300

    EL-42 | Add schema for Election

--- libs/models/Election.js
+++ libs/models/Election.js
@@ -1,8 +1,21 @@
 const mongoose = require('mongoose');
 
-const electionSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-});
+const electionSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    proposals: {
+      type: {
+        title: String,
+        options: [String],
+      },
+      required: true,
+    },
+    startAt: { type: Date, required: true },
+    endAt: { type: Date, required: true },
+  },
+  { timestamps: true },
+);
 
 const Election = mongoose.model('Election', electionSchema);
 
