c357c18d877fa4141e780f4fe5d7e05834c5dc6f
marcelodeleon <mdeleoncosentino@gmail.com>
2020-10-07 23:35:10 -0300

    EL-42 | Add schema for Election

--- functions/hello.js
+++ functions/hello.js
@@ -7,7 +7,13 @@ const mongodbUri = process.env.MONGODB_URI;
 exports.handler = async (event, context, callback) => {
   await mongodb(mongodbUri);
 
-  const election = new Election({ name: 'Test' });
+  const election = new Election({
+    name: 'TestN',
+    description: 'A desc for the test',
+    proposals: [{ title: 'newprop', options: ['opt1', 'opt2'] }],
+    startAt: new Date(),
+    endAt: new Date(),
+  });
   await election.save();
 
   callback(null, {
