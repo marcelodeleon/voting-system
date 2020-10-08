91a95f84b27e28cf282aec2880256e1b08d65dc9
marcelodeleon <mdeleoncosentino@gmail.com>
2020-10-07 21:35:51 -0300

    EL-27 EL28 | Configured mongo and mongoose

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,17 @@
-exports.handler = function (event, context, callback) {
+// TODO: Remove after adding the first endpoint!
+const { Election } = require('../libs/models');
+const { mongodb } = require('../libs/connectors');
+
+const mongodbUri = process.env.MONGODB_URI;
+
+exports.handler = async (event, context, callback) => {
+  await mongodb(mongodbUri);
+
+  const election = new Election({ name: 'Test' });
+  await election.save();
+
   callback(null, {
     statusCode: 200,
-    body: 'Hello, World'
+    body: 'Hello, World',
   });
 };
