745e6825b2d1b0880eef1a4a15fc46dfde5a1a3e
Marcelo de León <mdeleoncosentino@gmail.com>
2020-10-08 20:12:25 -0300

    EL-38 | Create Election screen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Vote from './components/users/Vote';
 import Election from './components/admin/Election';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
