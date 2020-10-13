7a8f2ee4b0d6ce2f211052882353c5cd7bf3cef8
Gonza <gonzalogg.garcia@gmail.com>
2020-10-13 01:40:42 -0300

    return data from database

--- src/components/users/Vote.js
+++ src/components/users/Vote.js
@@ -32,7 +32,6 @@ class Vote extends React.Component {
   };
 
   render() {
-<<<<<<< HEAD
     const { error, isLoaded, items } = this.state;
     if (error) {
       return <div>Error: {error.message}</div>;
@@ -70,27 +69,6 @@ class Vote extends React.Component {
         </React.Fragment>
       );
     }
-=======
-    // if (this.state.loding === true){
-    //   return 'Loading...';
-    // }
-    return (
-      <div>
-        <Navbar />
-        <CandidateList candidates={this.state.data} />
-        <div>
-          <a
-            className="App-link"
-            href="http://localhost:8888/.netlify/functions/getElections"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Votar
-          </a>
-        </div>
-      </div>
-    );
->>>>>>> 4796e54... backup before db changes
   }
 }
 export default Vote;
