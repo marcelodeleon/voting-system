15c328134fd76bb7d3e0d7d4b15827c5c2b37879
nrodrigoso <nolberquer@hotmail.com>
2020-10-19 23:38:23 -0300

    Added Modal

--- src/components/admin/Election.js
+++ src/components/admin/Election.js
@@ -1,15 +1,53 @@
 import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
+import ModalProposal from '../admin/ModalProposal';
 
 export default function Election() {
   const [name, setName] = useState('');
   const [startAt, setStartAt] = useState(new Date());
   const [endAt, setEndAt] = useState(new Date());
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  var [proposalsList, setProposals] = useState([]);
+  var proposalsObj = {};
+
+  const loadProposals = (data) => {
+    data.forEach((item) => {
+      if (
+        proposalsList.filter((elem) => elem['title'] === item['title'])
+          .length === 0
+      ) {
+        setProposals((proposalsList = proposalsList.concat(item)));
+      }
+    });
+    loadProposalsObj(proposalsList);
+    console.log(proposalsObj);
+  };
+
+  const loadProposalsObj = (data) => {
+    let obj = {};
+    for (var i = 0; i < data.length; i++) {
+      obj[i] = data[i];
+    }
+    proposalsObj = obj;
+  };
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
     /* eslint-disable-next-line */
     console.log('Submitting');
+  };
+
+  const openModal = () => {
+    setIsOpenModal(true);
+  };
+
+  const closeModal = () => {
+    setIsOpenModal(false);
+  };
+
+  const cancelModal = (event) => {
+    event.preventDefault();
+    setIsOpenModal(false);
   };
   return (
     <div>
@@ -44,6 +82,14 @@ export default function Election() {
         </label>
         <input type="submit" value="Submit" />
       </form>
+      <button onClick={openModal}>Crear Propuestas</button>
+      {isOpenModal && (
+        <ModalProposal
+          closeModal={closeModal}
+          cancelModal={cancelModal}
+          sendProposals={loadProposals}
+        />
+      )}
     </div>
   );
 }
