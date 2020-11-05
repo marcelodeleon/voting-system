8e3fa02cd1c9d5f7e892fb3e79809339706f41c5
Marcelo de León <mdeleoncosentino@gmail.com>
2020-11-05 19:59:12 -0300

    WIP

--- functions/getElectionsById.js
+++ functions/getElectionsById.js
@@ -1,18 +1,35 @@
+const mongoose = require('mongoose');
 const { Election } = require('../libs/models');
 const { mongodb } = require('../libs/connectors');
 
+const { Types } = mongoose;
 const mongodbUri = process.env.MONGODB_URI;
 
-exports.handler = async (event) => {
+exports.handler = async (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  console.log({ test: event.queryStringParameters });
+
+  const { electionId } = event.queryStringParameters;
+
   await mongodb(mongodbUri);
 
-  const election = await Election.findOne({});
+  try {
+    const election = await Election.findOne({
+      electionId: Types.ObjectId(electionId),
+    });
 
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(election),
-  };
+    return callback(null, {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(election),
+    });
+  } catch (error) {
+    return callback(null, {
+      statusCode: 400,
+      body: JSON.stringify({ error: 'No existe esa eleccion' }),
+    });
+  }
 };
