412153625787de423cf22cf7789aa67096c00b91
Marcelo de León <mdeleoncosentino@gmail.com>
2020-11-05 19:26:08 -0300

    EL-12 | Finished Voting

--- functions/createResult.js
+++ functions/createResult.js
@@ -1,30 +1,31 @@
-// TODO: Remove after adding the first endpoint!
+const mongoose = require('mongoose');
 const { Result } = require('../libs/models');
 const { mongodb } = require('../libs/connectors');
 
+const { Types } = mongoose;
 const mongodbUri = process.env.MONGODB_URI;
 
 exports.handler = async (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
   await mongodb(mongodbUri);
+  const { body } = event;
 
-  const { resultData } = JSON.parse(event.body);
+  const resultData = JSON.parse(body);
+  const { electionId, proposals } = resultData;
 
-  const result = new Result(resultData);
+  const updateVotes = {};
+  // eslint-disable-next-line
+  for (const [title, option] of Object.entries(proposals)) {
+    updateVotes[`proposals.${title}.${option}`] = 1;
+  }
 
-  await result
-    .save()
-    .then((doc) => {
-      return callback(null, {
-        statusCode: 200,
-        body: JSON.stringify(doc),
-      });
-    })
-    .catch((err) => {
-      console.error(err);
-      return callback(null, {
-        statusCode: 400,
-        body: JSON.stringify(error),
-      });
-    });
+  await Result.findOneAndUpdate(
+    { electionId: Types.ObjectId(electionId) },
+    { $inc: updateVotes },
+  );
+
+  return callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({ msg: 'ok' }),
+  });
 };
