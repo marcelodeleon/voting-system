b0b65eea540d0d5fe75f573ddb3424d6cd2bdb4c
marcelodeleon <mdeleoncosentino@gmail.com>
2020-11-05 00:06:08 -0300

    EL-4 EL-5 | User registration first draft

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import LogIn from './components/users/LogIn';
+import UserRegistration from './components/users/UserRegistration';
 import Vote from './components/users/Vote';
 import Election from './components/admin/Election';
 
@@ -10,6 +11,7 @@ function App() {
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={LogIn} />
+        <Route exact path="/users/registration" component={UserRegistration} />
         <Route exact path="/vote" component={Vote} />
         <Route exact path="/admin/election" component={Election} />
       </Switch>
