be5adbf29b6bb436ea069b4b33bfd6097636241a
marcelodeleon <mdeleoncosentino@gmail.com>
2020-11-06 01:23:39 -0300

    Link to user registration

--- src/components/users/LogIn.js
+++ src/components/users/LogIn.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import styled from 'styled-components';
-import { useHistory } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
 import apiClient from '../../utils/api-client';
 import { setSessionToken } from '../../utils/session';
 
@@ -14,6 +15,9 @@ const LogInForm = styled.form`
 `;
 
 const LogInFields = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   border: 1px solid teal;
   border-radius: 1rem;
   padding: 3rem 2rem;
@@ -32,12 +36,12 @@ const SubmitInput = styled.input`
 `;
 
 export default function LogIn() {
+  const { register, handleSubmit, errors } = useForm();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const history = useHistory();
 
-  const handleSubmit = async (evt) => {
-    evt.preventDefault();
+  const onSubmit = async () => {
     try {
       const { token } = await apiClient.post('sessions', {
         body: {
@@ -54,26 +58,42 @@ export default function LogIn() {
   };
 
   return (
-    <LogInForm onSubmit={handleSubmit}>
+    <LogInForm onSubmit={handleSubmit(onSubmit)}>
       <h1>Login</h1>
       <LogInFields>
         <label>
           Email:
           <input
             type="email"
+            name="email"
+            ref={register({ required: true })}
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
+          {errors.email && <span className="error">Campo requerido!</span>}
         </label>
         <label>
           Password:
           <input
-            type="text"
+            type="password"
             value={password}
+            name="password"
+            ref={register({ required: true, minLength: 8 })}
             onChange={(e) => setPassword(e.target.value)}
           />
+          {errors.password && errors.password.type === 'required' && (
+            <span className="error">Campo requerido!</span>
+          )}
+          {errors.password && errors.password.type === 'minLength' && (
+            <span className="error">
+              La contraseña debe tener al menos 8 caracteres.
+            </span>
+          )}
         </label>
         <SubmitInput type="submit" value="Log In" />
+        <Link to={'/users/registration'}>
+          ¿No tienes cuenta? Click aquí para registrar un usuario
+        </Link>
       </LogInFields>
     </LogInForm>
   );
