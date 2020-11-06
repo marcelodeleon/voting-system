9718da5be9d1cc29fdb2ba0f7a14eecc9c302d7a
Gonza <gonzalogg.garcia@gmail.com>
2020-11-05 23:55:37 -0300

    Sending email done

--- functions/createElections.js
+++ functions/createElections.js
@@ -1,5 +1,6 @@
 const { Election } = require('../libs/models');
 const { mongodb } = require('../libs/connectors');
+const sendEmail = require('../src/utils/sendEmail');
 
 const mongodbUri = process.env.MONGODB_URI;
 
@@ -8,10 +9,17 @@ exports.handler = async (event, context, callback) => {
   await mongodb(mongodbUri);
 
   const { electionData } = JSON.parse(event.body);
-
+  console.log(electionData);
   const election = new Election(electionData);
-  await election.save();
 
+  sendEmail(
+    'gonzalogg.garcia@gmail.com',
+    'hola',
+    '<strong>esto es un mail</strong>',
+    new Date(electionData.startAt),
+  );
+
+  await election.save();
   return callback(null, {
     statusCode: 200,
     body: JSON.stringify({ msg: 'ok' }),
