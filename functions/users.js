4ff7ede259ecb6a9b42dad09f0b8f97cfa4d553e
Federico <falberti@asapdevelopers.com>
2020-11-06 22:48:38 -0300

    finished email verification

--- functions/users.js
+++ functions/users.js
@@ -1,7 +1,14 @@
 const { User } = require('../libs/models');
 const { mongodb } = require('../libs/connectors');
 
+const sendEmail = require('../src/utils/sendEmail');
 const mongodbUri = process.env.MONGODB_URI;
+const { NODE_ENV } = process.env;
+
+const urlOrigin =
+  NODE_ENV === 'development'
+    ? 'https://voting-system-tas.netlify.app/verify?userId='
+    : 'http://localhost:8888/verify?userId=';
 
 exports.handler = async (event, context) => {
   context.callbackWaitsForEmptyEventLoop = false;
@@ -13,6 +20,17 @@ exports.handler = async (event, context) => {
 
   const newUser = new User(usersData);
   await newUser.save();
+
+  sendEmail(
+    [newUser.email],
+    'Validacion de usuario.',
+    '<strong>Valida tu usuario haciendo click </strong><a href=' +
+      urlOrigin +
+      newUser._id +
+      '&tokenId=' +
+      newUser.emailVerificationToken +
+      '>aquí</a>',
+  );
 
   return {
     statusCode: 200,
