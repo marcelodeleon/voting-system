1c1e82d7d6241113be29dc4dac31ebd0d38b07d5
Marcelo de León <mdeleoncosentino@gmail.com>
2020-11-12 17:09:02 -0300

    Tests

--- src/components/admin/Election.spec.js
+++ src/components/admin/Election.spec.js
@@ -1,29 +1,13 @@
 import React from 'react';
-import { render, fireEvent, waitFor, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
 import 'mutationobserver-shim';
 import Election from './Election';
-// import { isAuthenticated } from '../utils/auth';
 
 global.MutationObserver = window.MutationObserver;
-// jest.mock('../utils/auth');
 
 describe('Election', () => {
-  beforeEach(() => {
-    // isAuthenticated.mockRestore()
-  })
-
   test('Renders Election correctly', () => {
     const { container } = render(<Election />);
     expect(container.firstChild).toMatchSnapshot();
   });
-
-  test.only('test', async () => {
-    render(<Election />);
-    fireEvent.click(screen.getByText(/submit/i))
-
-    // const test = await waitFor(() => screen.getByText(/error/i))
-
-    console.log({ test })
-  })
 });
